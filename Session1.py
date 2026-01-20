7f5dcf6cce36ecdaa013af5c439b32726964a4a9
saraalshurafa <saraalshurafa00@gmail.com>
2026-01-20 16:59:30 +0100

    Mt first commit ever

---
+++ Session1.py
@@ -0,0 +1,18 @@
+#this is my first code
+print("Hello world")
+print(7+6)
+# print("7+6") u do command slash to get the hashtag
+print('and jo asked: "how r u?"')
+print("hello"" ""world")
+print("3 + 4 =", 3+4)
+print("3 + 4 =", 3+4, " and 5 x 6 =", 5*6)
+# what if we dont want the spaces
+print("I have $", 100+200, ".")
+print("I have $", 100+200, ".", sep="!!")
+print("1", end="")
+print("2", end="")
+print("3", end= "!!")
+# what if u wanted to leave a space in ur code when u run it...
+print("4", end="\n\n\n")
+print("5")
+
